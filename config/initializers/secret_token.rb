523b528e423f3472b08e1d27a06b45c1e7ea83d4
石川佳実 <si12002@edu.aomori-u.ac.jp>
2015-06-08 11:58:35 +0900

    初期化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ObuchiApp::Application.config.secret_key_base = 'f50baebf4172decf8461f7ef9e5724814789a1ed0fbfd4990a8154430432d0b9af6449e9f80dc4953feb9c4bac9ddc2de6ae81f8e9f0ba87b0ce2772ec9b9a85'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ObuchiApp::Application.config.session_store :cookie_store, key: '_obuchi_app_session'
