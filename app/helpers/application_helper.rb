72ac348781b8b6acd7ec61e959316f15d2c9f235
石川佳実 <si12002@edu.aomori-u.ac.jp>
2015-06-08 18:45:53 +0900

    full_titleヘルパーを定義

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	# ページごとの完全なタイトルを返します。
+  	def full_title(page_title)
+    	base_title = "Obuchi App"
+    	if page_title.empty?
+      	base_title
+    else
+      	"#{base_title} | #{page_title}"
+    end
+  end
 end
