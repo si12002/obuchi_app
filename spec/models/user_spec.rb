fac098932dee6e0af01b0b0f87682bdf763b231a
石川佳実 <si12002@edu.aomori-u.ac.jp>
2015-06-09 17:37:03 +0900

    ユーザーの検証

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,68 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+  	@user = User.new(name: "Example User", email: "user@example.com") 
+  end
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  # name属性の存在性を調べる
+  describe "when name is not present" do
+  	# nameが空欄だったら
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+
+  # email属性の存在性を調べる
+  describe "when email is not present" do
+  	# emailが空欄だったら
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
+  # nameの長さを調べる
+  describe "when name is too long" do
+  	# 名前が長過ぎたら
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
+
+  # メールアドレスのフォーマットを調べる
+  describe "when email format is invalid" do
+    # メールアドレスが無効だったとき
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+  describe "when email format is valid" do
+  	# メールアドレスが有効だったとき
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end
+
+  # 重複するメールアドレスの拒否のテスト
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase # 大文字小文字を区別しない
+      user_with_same_email.save
+    end
+    it { should_not be_valid }
+  end
+
 end
