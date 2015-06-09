a2edd96dc23b43701e3c68d9c0aedb8e6875fff4
石川佳実 <si12002@edu.aomori-u.ac.jp>
2015-06-09 19:27:21 +0900

    パスワードの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,38 +2,42 @@ require 'spec_helper'
 
 describe User do
   before do
-  	@user = User.new(name: "Example User", email: "user@example.com") 
+  	@user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") 
   end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
-  # name属性の存在性を調べる
+### name ###
+  ## name属性の存在性を調べる
   describe "when name is not present" do
   	# nameが空欄だったら
     before { @user.name = " " }
     it { should_not be_valid }
   end
-
-  # email属性の存在性を調べる
-  describe "when email is not present" do
-  	# emailが空欄だったら
-    before { @user.email = " " }
-    it { should_not be_valid }
-  end
-
-  # nameの長さを調べる
+  ## nameの長さを調べる
   describe "when name is too long" do
   	# 名前が長過ぎたら
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
 
-  # メールアドレスのフォーマットを調べる
+### email ###
+  ## email属性の存在性を調べる
+  describe "when email is not present" do
+  	# emailが空欄だったら
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+  ## メールアドレスのフォーマットを調べる
   describe "when email format is invalid" do
     # メールアドレスが無効だったとき
     it "should be invalid" do
@@ -54,8 +58,7 @@ describe User do
       end
     end
   end
-
-  # 重複するメールアドレスの拒否のテスト
+  ## 重複するメールアドレスを拒否するためのテスト
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
@@ -63,6 +66,44 @@ describe User do
       user_with_same_email.save
     end
     it { should_not be_valid }
+  end
+
+### password ###
+  ## パスワードとパスワード確認のテスト
+  describe "when password is not present" do
+  	# パスワードが(入力されて)なかったら
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+  describe "when password doesn't match confirmation" do
+  	# パスワードが確認パスワードと一緒じゃなかったら
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+  ## パスワードの長さとauthenticateメソッドのテスト
+  describe "with a password that's too short" do
+  	# passwordが短かったとき
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+  describe "return value of authenticate method" do
+  	# authenticateメソッドの値を返したとき
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+    describe "with valid password" do
+    	#パスワードが有効だったら
+    	it { should eq found_user.authenticate(@user.password) }
+    end
+    describe "with invalid password" do
+    	# パスワードが無効だったら
+      	let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      	it { should_not eq user_for_invalid_password }
+      	specify { expect(user_for_invalid_password).to be_false }
+    end
   end
 
 end
