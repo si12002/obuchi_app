8be0d9ae3ce5abd29b62acc8f99b5cf08cecdce2
石川佳実 <si12002@edu.aomori-u.ac.jp>
2015-06-10 18:57:23 +0900

    ユーザー登録が完了したときの動作を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		# 保存の成功をここで扱う
+  		flash[:success] = "登録完了しました！ようこそ！"
+  		redirect_to @user
   	else
   		render 'new'
   	end
